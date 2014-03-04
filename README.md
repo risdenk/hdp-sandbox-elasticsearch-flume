dd4627f342b56082c332730b0c154ee1baa76b3d
Kevin Risden <risdenk@avalonconsult.com>
2014-03-04 14:40:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# hdp-sandbox-elasticsearch-flume
+Hortonworks Blog Post - http://hortonworks.com/blog/configure-elastic-search-hadoop-hdp-2-0/
+
+##Getting Started
+### Clone the Repo
+1. `git clone https://github.com/risdenk/hdp-sandbox-elasticsearch-flume.git`
+2. `cd hdp-sandbox-elasticsearch-flume`
+
+### Run the install
+1. `sudo ./install.sh`
+
+### Setup Port Forwarding
+Elasticsearch requires port 9200 to be forwarded to use the http interface for Kibana
+
+### View the Result
+1. Open [http://localhost:42080/kibana](http://localhost:42080/kibana)
+*** By default port 80 is forwarded to 42080 on the HDP 2.0 Sandbox
---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+set -e 
+set -o pipefail
+
+# Install Elasticsearch
+ES_VER="1.0.1"
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${ES_VER}.noarch.rpm
+rpm -ivh elasticsearch-${ES_VER}.noarch.rpm
+
+# Configure Elasticsearch
+cp elasticsearch.yml /etc/elasticsearch/
+service elasticsearch restart
+
+# Download Kibana
+KIBANA_HOME="/opt/kibana"
+KIBANA_VER="3.0.0milestone5"
+wget https://download.elasticsearch.org/kibana/kibana/kibana-${KIBANA_VER}.tar.gz
+tar -C /opt/ -zxvf kibana-${KIBANA_VER}.tar.gz
+ln -s /opt/kibana-${KIBANA_VER} $KIBANA_HOME
+
+# Configure Kibana
+cp config.js $KIBANA_HOME
+cp flume.json ${KIBANA_HOME}/app/dashboards/
+
+# Install Flume
+yum install -y flume-agent flume
+
+# Setup Flume
+FLUME_HOME=/usr/lib/flume
+ES_HOME=/usr/share/elasticsearch
+
+mkdir -p ${FLUME_HOME}/plugins.d/elasticsearch/lib
+
+cp ${ES_HOME}/lib/elasticsearch-*.jar ${FLUME_HOME}/plugins.d/elasticsearch/lib/
+cp ${ES_HOME}/lib/lucene-*.jar ${FLUME_HOME}/plugins.d/elasticsearch/lib/
+
+# FIX for https://issues.apache.org/jira/browse/FLUME-2174
+rm -f ${FLUME_HOME}/lib/lucene-*.jar
+
+#Update Flume configuration to consume a local file and index into
+#Elasticsearch in logstash format.
+FLUME_CONF=/etc/flume/conf/
+cp flume.conf $FLUME_CONF
+
+# Add test data to /tmp/
+cp es_log.log /tmp/
+
+service flume-agent start
+
+# Setup Kibana Web Server
+echo "Alias /kibana /opt/kibana" > /etc/httpd/conf.d/kibana.conf
+service httpd restart
